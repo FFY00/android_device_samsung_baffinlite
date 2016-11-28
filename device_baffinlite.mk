e9ad0e2ce909fa5b9db908e628116a3dbeda3733
Grace5921 <androidlover5842@gmail.com>
2016-11-28 20:58:25 +0530

    update vendor makefile path

--- device_baffinlite.mk
+++ device_baffinlite.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product-if-exists, vendor/samsung/baffinlite/baffinlite-common-vendor.mk)
+$(call inherit-product, vendor/samsung/baffinlite/baffinlite-vendor.mk)
 
 # Use high-density artwork where available
 PRODUCT_LOCALES += hdpi
